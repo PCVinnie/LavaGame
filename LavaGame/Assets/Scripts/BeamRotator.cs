e55ac2374d1f93128c3b9cac768a7f371395fac9
PCVinnie <vastout@vasnet.nl>
2015-12-16 15:09:47 +0100

    Added new map to project, textures and script

--- LavaGame/Assets/Scripts/BeamRotator.cs
+++ LavaGame/Assets/Scripts/BeamRotator.cs
@@ -6,6 +6,6 @@ public class BeamRotator : MonoBehaviour {
     // Use this for initialization
     void Update()
     {
-        transform.Rotate(new Vector3(0, 360, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);
     }
 }
