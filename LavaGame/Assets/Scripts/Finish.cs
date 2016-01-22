c22486a9a841a460d0dd3ba92609dad0d4489df0
pjcbakx <patrickbakx95@hotmail.com>
2016-01-22 14:46:33 +0100

    When player touches finish, game goes back to menu

---
+++ LavaGame/Assets/Scripts/Finish.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
